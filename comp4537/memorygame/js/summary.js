1030d80049f357dbdb2c6ac8544fed215f740557
victorwu <victor__wu@live.com>
2020-10-31 23:11:42 -0700

    complete

--- comp4537/memorygame/js/summary.js
+++ comp4537/memorygame/js/summary.js
@@ -7,7 +7,7 @@ async function submit() {
     let urlParams = new URLSearchParams(window.location.search)
     let score = urlParams.get("score")
     let url = `https://memgame-server.herokuapp.com/insert_score?name=${name}&score=${score}`
-    let res = await (await fetch(url)).json()
+    let res = await (await fetch(url, {mode: "cors"})).json()
     // res = await res.json()
     console.log(res)
 
